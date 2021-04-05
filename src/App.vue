eaf95bc7b5c217e3ff34c8207cbc3b74387080f8
Leo Zeng <qingchun1995@gmail.com>
2021-04-05 22:23:38 +0800

    change: App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
     <!-- <h4>{{ path }}</h4> -->
 
     <div class="form-group mt-4 mb-2">
-      <input class="form-control form-control-sm" placeholder="File search" />
+      <input class="form-control form-control-sm" v-model="searchString" placeholder="File search" />
     </div>
   </div>
 </template>
 
 <script>
 import fs from 'fs-extra'
-// import path from 'path'
+import path from 'path'
 
 import { app } from '@electron/remote'
 import { ref, computed } from 'vue'
@@ -18,29 +18,41 @@ console.info(app.getAppPath())
 export default {
   name: 'App',
   setup () {
-    const { value: appPath } = ref(app.getAppPath())
+    const appPath = ref(app.getAppPath())
     const files = computed(() => {
-      // Return an array of file names
-      const fileNames = fs.readdirSync(appPath)
+      // Return an array of file names for all the files and directories in a given path
+      const fileNames = fs.readdirSync(appPath.value)
+      const result = fileNames.map((file) => {
+        const stats = fs.statSync(path.join(appPath.value, file))
 
-      console.info(fileNames)
+        return {
+          name: file,
+          size: stats.isFile() ? stats.size ?? 0 : null,
+          directory: stats.isDirectory()
+        }
+      })
 
-      return {}
-
-      // return fileNames.map((file) => {
-      //   const stats = fs.statSync(path.join(appPath, file))
-
-      //   return {
-      //     name: file,
-      //     size: stats.isFile() ? stats.size ?? 0 : null,
-      //     directory: stats.isDirectory()
-      //   }
-      // })
+      return result
     })
+    console.info('Files:', files.value)
+    const searchString = ref('')
 
-    console.info('Test leo --- show files', files.value)
+    function open (folder) {
+      // The path.join() method joins all given path segments together using the platform-specific separator as a delimiter, 
+      // then normalizes the resulting path.
+      appPath.value = path.join(appPath.value, folder)
+    }
+    function back () {
+      appPath.value = path.dirname(appPath.value)
+    }
 
-    return {}
+    return {
+      files,
+      searchString,
+
+      open,
+      back
+    }
   }
 }
 </script>
